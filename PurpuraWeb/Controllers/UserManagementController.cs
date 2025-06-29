f6bc7ce6f4ec26cd61a7fdc3628419aa8784ba05
RoKen1000 <rosskennedy993@outlook.com>
2025-06-29 11:27:28 +0100

    Simplify method names. Remove user management side bar partial as it is no longer needed. Refactor CSS.

--- PurpuraWeb/Controllers/UserManagementController.cs
+++ PurpuraWeb/Controllers/UserManagementController.cs
@@ -5,6 +5,7 @@ using Purpura.Utility.Helpers;
 
 namespace PurpuraWeb.Controllers
 {
+    [Route("[controller]")]
     public class UserManagementController : Controller
     {
         private readonly IUserManagementService _userManagementService;
@@ -28,7 +29,7 @@ namespace PurpuraWeb.Controllers
 
         [HttpGet]
         [Route("Edit/{userId}")]
-        public async Task<IActionResult> EditUserDetails(string userId)
+        public async Task<IActionResult> Edit(string userId)
         {
             var userViewModel = await _userManagementService.GetUser(u => u.Id == userId);
 
@@ -44,7 +45,7 @@ namespace PurpuraWeb.Controllers
 
         [HttpPost]
         [Route("Edit/{userId}")]
-        public async Task<IActionResult> EditUserDetails(ApplicationUserViewModel viewModel)
+        public async Task<IActionResult> Edit(ApplicationUserViewModel viewModel)
         {
             if (ModelState.IsValid)
             {
