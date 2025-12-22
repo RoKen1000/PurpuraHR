ebf59cda1c9173dda9e6b6265bade0ce44ca0c82
RoKen1000 <rosskennedy993@outlook.com>
2025-12-22 13:59:23 +0000

    Create validation method for emails on form.

--- Purpura.Models/ViewModels/CompanyEmployeeViewModel.cs
+++ Purpura.Models/ViewModels/CompanyEmployeeViewModel.cs
@@ -1,11 +1,22 @@
-﻿namespace Purpura.Models.ViewModels
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Purpura.Models.ViewModels
 {
     public class CompanyEmployeeViewModel : BaseViewModel
     {
+        [Required]
         public string FirstName { get; set; }
         public string? MiddleName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string JobTitle { get; set; }
         public string CompanyExternalReference { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public bool EmailExists { get; set; }
     }
 }
--- PurpuraWeb/wwwroot/js/CompanyEmployee.js
+++ PurpuraWeb/wwwroot/js/CompanyEmployee.js
@@ -1,1 +1,41 @@
 ﻿
+function initialiseEmailCheckAjax(url) {
+    $("#Email").on("focusout", function () {
+        const $emailExistsField = $("#EmailExists");
+        $emailExistsField.val(false);
+
+        const $validationDiv = $("#existing-email-validation");
+        $validationDiv.css({ "margin-bottom": "10px" });
+        $validationDiv.html(`<div class="d-flex justify-content-center">
+                                <div class="spinner-border" style="color: mediumpurple;" role="status">
+                                    <span class="visually-hidden">Loading...</span>
+                                </div>
+                            </div>`);
+
+        if (emailValid($(this).val())) {
+            $.post(url, { input: $(this).val() }, function (exists) {
+                if (exists) {
+                    $validationDiv.html("<i class='bi bi-check' style='color: green; font-size: large;'></i> Email exists and employee will be assigned to the associated user.");
+                    $emailExistsField.val(true);
+                }
+                else {
+                    $validationDiv.html("<i class='bi bi-info-circle'></i> Email does not exist. If employee is created now then the user will be automatically assigned to this company after registering.");
+                }
+            });
+        }
+        else {
+            $validationDiv.html("");
+            return;
+        }
+    });
+}
+
+function emailValid(value) {
+    if (value == null || !value.length) {
+        return false;
+    }
+
+    const regex = /^(\w{1,}|\w{1,}\.\w{1,}|\w{1,}\.\w{1}\.\w{1,})@\w{2,}\.(\w{2}\.\w{2}|\w{2,})$/;
+
+    return regex.test(value);
+}
